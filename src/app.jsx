5d7c2110a307a1f7af699451a4a9351da4968769
Seunghyo <seunghyoku@gmail.com>
2021-02-17 15:58:37 +0900

    refactor: 필요없는 기본 css 파일 삭제

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,37 @@
-import "./app.css";
+import React, { useState, useEffect } from "react";
+import Header from "./components/header/header";
+import Body from "./components/body/body";
 
-import Header from "./components/header";
+require("dotenv").config();
+const axios = require("axios");
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  const getVideos = async () => {
+    const maxResults = 24;
+    try {
+      await axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/videos?key=${process.env.REACT_APP_API_KEY}&part=snippet&chart=mostPopular&maxResults=${maxResults}`
+        )
+        .then((result) => {
+          const { items } = result.data;
+          setVideos(items);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <>
       <Header />
+      <Body videoList={videos} />
     </>
   );
 }
