a6ac0995da0a0bcee7050b8d63d8ddf341cfc1b0
Seunghyo <seunghyoku@gmail.com>
2021-02-18 17:59:51 +0900

    feat: 말 줄임표 구현 #1 - 기존에 구현했던 방법은 글자 개수로 특정 글자 수 이상이 될 경우 ...를 추가하는 방법이었는데 이 경우 반응형으로 구현하기 어려웠다. - 예를 들어 화면을 줄이면 가로폭이 더 좁아져야 되는데 글자수가 전체화면 대비 더 좁기 때문에 3줄 이상으로 나오기도 했다. - 이번에 적용한 방법은 css를 이용한 방법인데 webkit을 익스플로러에서는 사용하지 않기 떄문에 적용이 안된다는 듯.. 그래도 한 번 써보았다.

--- src/components/videoItem/videoItem.jsx
+++ src/components/videoItem/videoItem.jsx
@@ -5,14 +5,6 @@ import getElapsedTime from "../../utils/getElapsedTime";
 const Video = (props) => {
   //const { id } = props.video;
   const { thumbnails, title, channelTitle, publishedAt } = props.video;
-
-  const applyEllipsis = (title) => {
-    const maxString = 77;
-    return title.length > maxString
-      ? title.substr(0, maxString - 3) + "..."
-      : title;
-  };
-
   // 225 : 202
   return (
     <li className={styles.video}>
@@ -22,7 +14,7 @@ const Video = (props) => {
         alt="thumbnails"
       ></img>
       <div className={styles.videoInfo}>
-        <h3 className={styles.title}>{applyEllipsis(title)}</h3>
+        <h3 className={styles.title}>{title}</h3>
         <p className={styles.channelTitle}>{channelTitle}</p>
         <p className={styles.elapsedTime}>{getElapsedTime(publishedAt)}</p>
       </div>
