db5237f816e258e6089246585866458574cd3e50
Seunghyo <seunghyoku@gmail.com>
2021-02-22 18:50:49 +0900

    feat: 검색 화면 구현 #4 - 하루 이내 올린 동영상의 경우 새 동영상 < 태그가 나오도록 함 - 채널의 경우 다른 디자인으로 출력되게끔 함 - searched되었을 경우, 일반 비디오와 다른 화면으로 보이게끔 출력

--- src/components/body/videoItem/videoItem.jsx
+++ src/components/body/videoItem/videoItem.jsx
@@ -3,10 +3,54 @@ import styles from "./videoItem.module.css";
 import getElapsedTime from "../../../utils/getElapsedTime";
 
 const Video = (props) => {
-  const { thumbnails, title, channelTitle, publishedAt } = props.video;
-  const menuOpened = props.menuOpened;
+  const {
+    thumbnails,
+    title,
+    channelTitle,
+    publishedAt,
+    description,
+  } = props.video.snippet;
+  const { kind } = props.video.id;
+  const isVideo = kind && /#([a-zA-Z]*)/.exec(kind)[1] === "video";
+  const { menuOpened, searched } = props;
+  const isNewVideo = publishedAt && /시간/.exec(getElapsedTime(publishedAt));
 
-  return (
+  return searched ? (
+    <li className={styles.searchedVideo}>
+      {isVideo ? (
+        <img
+          className={styles.searchedThumbnails}
+          src={thumbnails.medium.url}
+          alt="thumbnails"
+        ></img>
+      ) : (
+        <div className={styles.imageBox}>
+          <img
+            className={styles.channelProfile}
+            src={thumbnails.medium.url}
+            alt="channelProfile"
+          ></img>
+        </div>
+      )}
+
+      <div className={styles.videoInfo + " " + styles.videoInfoSearched}>
+        <h3 className={styles.searchedTitle}>{title}</h3>
+        {isVideo ? <p className={styles.channelTitle}>{channelTitle}</p> : ""}
+        {isVideo ? (
+          <p className={styles.elapsedTime}>{getElapsedTime(publishedAt)}</p>
+        ) : (
+          ""
+        )}
+        <p className={styles.description}>{description}</p>
+        {isNewVideo ? (
+          <span className={styles.newVideoTag}>새 동영상</span>
+        ) : (
+          ""
+        )}
+      </div>
+      {!isVideo ? <button className={styles.subscribe}>구독</button> : ""}
+    </li>
+  ) : (
     <li
       className={
         menuOpened ? styles.video : styles.video + " " + styles.videoClosed
