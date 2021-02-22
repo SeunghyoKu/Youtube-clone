4927fd36e1a0961fbd25522143462052010b5971
Seunghyo <seunghyoku@gmail.com>
2021-02-22 19:15:21 +0900

    feat: 시간 유틸 함수 몇 분전인지까지 출력되게끔 지원함

--- src/utils/getElapsedTime.js
+++ src/utils/getElapsedTime.js
@@ -10,8 +10,10 @@ const elapsedTime = (publishedAt) => {
     return `${Math.floor(elapsedDate / 7)}주 전`;
   } else if (elapsedDate > 1) {
     return `${Math.floor(elapsedDate)}일 전`;
-  } else {
+  } else if (elapsedDate * 24 > 1) {
     return `${Math.floor(elapsedDate * 24)}시간 전`;
+  } else {
+    return `${Math.floor(elapsedDate * 24 * 60)}분 전`;
   }
 };
 
