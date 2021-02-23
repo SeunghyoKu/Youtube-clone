20d7b19640a2d4a50b07c91529bc39f89b3568e0
Seunghyo <seunghyoku@gmail.com>
2021-02-23 20:04:36 +0900

    style: 비디오 누를 시, 왼쪽에 뜨게끔 함 #6 - 나중에 videoList는 수정해야할 것으로 보이나 누르면 일단 menu 안 보이고 크게 비디오가 보이게끔 구현 :

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -7,15 +7,27 @@ import styles from "./body.module.css";
 const Body = (props) => {
   return (
     <body className={styles.body}>
-      <Menu menuOpened={props.menuOpened} />
-      {!!props.videoOpened ? <VideoDetail id={props.videoOpened} /> : ""}
-      <Videos
-        videoList={props.videoList}
-        menuOpened={props.menuOpened}
-        searched={props.searched}
-        videoOpened={props.videoOpened}
-        setVideoOpened={props.setVideoOpened}
-      />
+      {!props.videoOpened ? <Menu menuOpened={props.menuOpened} /> : ""}
+      <section
+        className={
+          !!props.videoOpened
+            ? styles.sectionVideoOpened
+            : styles.sectionVideoClosed
+        }
+      >
+        {!!props.videoOpened ? (
+          <VideoDetail id={props.videoOpened} menuOpened={props.menuOpened} />
+        ) : (
+          ""
+        )}
+        <Videos
+          videoList={props.videoList}
+          menuOpened={props.menuOpened}
+          searched={props.searched}
+          videoOpened={props.videoOpened}
+          setVideoOpened={props.setVideoOpened}
+        />
+      </section>
     </body>
   );
 };
--- src/components/body/videoDetail/videoDetail.jsx
+++ src/components/body/videoDetail/videoDetail.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import styles from "./videoDetail.module.css";
 
-const VideoDetail = ({ id }) => {
+const VideoDetail = ({ id, menuOpened }) => {
+  console.log(menuOpened);
   return (
-    <>
-      <iframe
-        title="hello"
-        className="ytplayer"
-        type="text/html"
-        width="720"
-        height="405"
-        src={`https://www.youtube.com/embed/${id}`}
-        frameborder="0"
-        allowfullscreen
-      ></iframe>
-    </>
+    <div className={styles.videoDetail}>
+      <div className={styles.video}>
+        <iframe
+          title="ytbVideo"
+          className="ytplayer"
+          type="text/html"
+          src={`https://www.youtube.com/embed/${id}`}
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
   );
 };
 
