b06f01b84b30b5064d03260b85e0b554f9590ed5
Seunghyo <seunghyoku@gmail.com>
2021-02-26 16:01:24 +0900

    style: 날짜 출력시 .이 덜 나와서 추가해줌 #6

--- src/utils/changeDate.js
+++ src/utils/changeDate.js
@@ -1,7 +1,7 @@
 const changeDate = (date) => {
   const MAX_DATE_LENGTH = 10;
   const changedDate = date.slice(0, MAX_DATE_LENGTH).split("-");
-  return changedDate.join(". ");
+  return changedDate.join(". ") + ".";
 };
 
 export default changeDate;
