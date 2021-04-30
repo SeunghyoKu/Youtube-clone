c3558f2d748edd10d56eead90de868fc2dc13b63
Seunghyo <seunghyoku@gmail.com>
2021-05-01 01:50:53 +0900

    chore: 리액트에 맞게 프로퍼티 변경

--- src/components/body/videoDetail/videoDetail.jsx
+++ src/components/body/videoDetail/videoDetail.jsx
@@ -22,8 +22,8 @@ const VideoDetail = ({ video, menuOpened }) => {
           className="ytplayer"
           type="text/html"
           src={`https://www.youtube.com/embed/${videoId}`}
-          frameborder="0"
-          allowfullscreen
+          frameBorder="0"
+          allowFullScreen
         ></iframe>
       </div>
       <div className={styles.videoInfo}>
