e4e67d4715b95ca847abdfd3d367ea6846785b06
Seunghyo <seunghyoku@gmail.com>
2021-05-03 15:45:43 +0900

    style: 메인, 메뉴, 헤더바 다크/라이트 모드 구현하기 #14 - 하지만 유튜브 로고도 이미지 바뀌도록 할 필요가 있음!

--- src/components/header/headerRightIcons.jsx
+++ src/components/header/headerRightIcons.jsx
@@ -3,7 +3,7 @@ import styles from "./header.module.css";
 
 const HeaderRightIcons = (props) => {
   return (
-    <div>
+    <div className={styles.headerRightIcons}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
