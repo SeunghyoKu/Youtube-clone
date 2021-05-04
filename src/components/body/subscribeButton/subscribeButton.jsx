dc68764c27a5364cea30ce8003fe3e65ee1462f2
Seunghyo <seunghyoku@gmail.com>
2021-05-05 00:20:20 +0900

    style: 버튼 css를 styled 로 변경하기 #17

--- src/components/body/subscribeButton/subscribeButton.jsx
+++ src/components/body/subscribeButton/subscribeButton.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./subscribeButton.module.css";
+import styled from "styled-components";
 
 const SubscribeButton = (props) => {
   const [subscribed, setSuscirbed] = useState(false);
@@ -10,20 +10,27 @@ const SubscribeButton = (props) => {
   };
 
   return subscribed ? (
-    <button
-      className={styles.subscribe + " " + styles.notSubscribed}
-      onClick={onClick}
-    >
+    <Button onClick={onClick} subscribed={subscribed}>
       구독
-    </button>
+    </Button>
   ) : (
-    <button
-      className={styles.subscribe + " " + styles.subscribed}
-      onClick={onClick}
-    >
+    <Button onClick={onClick} subscribed={subscribed}>
       구독중
-    </button>
+    </Button>
   );
 };
+
+const Button = styled.button`
+  height: 40px;
+  width: 72px;
+  color: white;
+  border: none;
+  border-radius: 5%;
+  padding: 10px 16px 10px 160x;
+  font-size: 14px;
+  margin: auto 0px auto 0px;
+  background-color: ${(props) =>
+    props.subscribed ? "rgba(255, 255, 255, 0.1)" : "red"};
+`;
 
 export default SubscribeButton;
