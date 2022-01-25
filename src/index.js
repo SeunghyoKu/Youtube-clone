dd6f419c68bb9e2031a13e63822a8cee6f0176e0
Seunghyo <seunghyoku@gmail.com>
2022-01-26 05:27:03 +0900

    Refactor: 좀 더 axios 스럽게 수정하기 - axios create를 이용하여 코드 간결화 - httpClient와 내부 로직을 뜯어내서 injection 방지

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app";
 import YoutubeAPI from "./service/youtubeAPI";
-const youtubeAPI = new YoutubeAPI();
+
+const axios = require("axios");
+require('dotenv').config();
+
+const httpClient = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {key: process.env.REACT_APP_API_KEY},
+})
+const youtubeAPI = new YoutubeAPI(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/service/youtubeAPI.js
+++ src/service/youtubeAPI.js
@@ -1,42 +1,31 @@
-const axios = require("axios");
-require("dotenv").config();
-
 class YoutubeAPI {
-  constructor() {
-    this.key = process.env.REACT_APP_API_KEY;
+  constructor(httpClient) {
+    this.youtube = httpClient;
     this.maxResults = 20;
   }
 
   // 인기 비디오 가져오기
   async getFamousVideos() {
-    return await axios
-      .get(
-        `https://www.googleapis.com/youtube/v3/videos?key=${this.key}&part=snippet&chart=mostPopular&maxResults=${this.maxResults}`
-      )
-      .then((result) => {
-        const { items } = result.data;
-        return items;
-      })
-      .catch((err) => {
-        console.error(err);
-        return [];
-      });
+    const response = await this.youtube.get('videos', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: this.maxResults,
+      }
+    })
+    return response.data.items;
   }
 
   // 키워드로 검색해 비디오 가져오기
   async getSearchWithText(searchText) {
-    return await axios
-      .get(
-        `https://www.googleapis.com/youtube/v3/search?key=${this.key}&part=snippet&maxResults=${this.maxResults}&q=${searchText}`
-      )
-      .then((result) => {
-        const { items } = result.data;
-        return items;
-      })
-      .catch((err) => {
-        console.error(err);
-        return [];
-      });
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: this.maxResults,
+        q: searchText
+      }
+    })
+    return response.data.items;
   }
 }
 
